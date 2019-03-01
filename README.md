939e9b56550faef4b7288c08aab70b87f146969f
Brandon Schmeidler <brandon.bsgstudio@gmail.com>
2019-03-01 00:12:46 -0600

    Discovered what how cool wxAuiManager is. Hellooo modern(ish) UI!
    Updated the metadata in source code
    Updated readme with terrible instructions
    Learned basics of Aui Managed elements

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Love2D-Editor
 This sucks. Don't use it.
+
+
+http://wxlua.sourceforge.net/download.php
+
+put wxLua.exe and wx.dll to be in same directory as source files and "wxLua main.lua" in command line to run program
+
+or just go away.
--- main.lua
+++ main.lua
@@ -1,17 +1,18 @@
 -----------------------------------------------------------------------------
--- Name:        minimal.wx.lua
--- Purpose:     'Minimal' wxLua sample
--- Author:      J Winwood
+-- Name:        Love2D Editor
+-- Purpose:    WSYIWYG Editor for Love2D
+-- Author:      Brandon Schmeidler
 -- Modified by:
--- Created:     16/11/2001
--- RCS-ID:      $Id: veryminimal.wx.lua,v 1.7 2008/01/22 04:45:39 jrl1 Exp $
--- Copyright:   (c) 2001 J Winwood. All rights reserved.
+-- Created:     2/28/2019
+-- RCS-ID:     I don't know what this is
+-- Copyright:   (c) 2019 B Schmeidler. All rights reserved.I guess.
 -- Licence:     wxWidgets licence
 -----------------------------------------------------------------------------
 
 -- Load the wxLua module, does nothing if running from wxLua, wxLuaFreeze, or wxLuaEdit
-package.cpath = package.cpath..";./?.dll;./?.so;../lib/?.so;../lib/vc_dll/?.dll;../lib/bcc_dll/?.dll;../lib/mingw_dll/?.dll;"
-require("wx")
+--package.cpath = package.cpath..";./?.dll;./?.so;../lib/?.so;../lib/vc_dll/?.dll;../lib/bcc_dll/?.dll;../lib/mingw_dll/?.dll;"
+--require("wx")
+--local Project = require("Project")
 
 frame = nil
 runningPID = 0
@@ -27,20 +28,24 @@ function StopProgram()
         print("No Program Running")
     end
 end
+
 function RunProgram()
     if (wx.wxProcess.Exists(runningPID)) then
         StopProgram()
     end
 
-    runningPID = wx.wxExecute("lovec", wx.wxEXEC_ASYNC, wx.wxProcess(frame))
+    runningPID = wx.wxExecute("love", wx.wxEXEC_ASYNC, wx.wxProcess(frame))
     if (runningPID > 0) then
         print("Program started succesfully -- PID: " .. tostring(runningPID))
+    else
+        print("Something went wrong. Make sure you have Love2D installed on your system and added to PATH")
     end
 end
 
 function CreateMenuBar()
     local fileMenu = wx.wxMenu()
-    fileMenu:Append(wx.wxID_EXIT, "Exit","Quit the program")
+    fileMenu:Append(wx.wxID_NEW, "New", "Start a new project")
+    fileMenu:Append(wx.wxID_EXIT, "&Exit\tAlt+F4","Quit the program")
 
     local projectMenu = wx.wxMenu()
     projectMenu:Append(69, "Run","Run the program")
@@ -54,13 +59,25 @@ function CreateMenuBar()
 end
 
 function main()
-
+    
     -- create the frame window
-    frame = wx.wxFrame( wx.NULL, wx.wxID_ANY, "wxLua Very Minimal Demo",
+    frame = wx.wxFrame( wx.NULL, wx.wxID_ANY, "Love2D Editor",
                         wx.wxDefaultPosition, wx.wxSize(640,480),
                         wx.wxDEFAULT_FRAME_STYLE )
     
     frame:SetMenuBar(CreateMenuBar())
+
+    local auiManager = wxaui.wxAuiManager(frame)
+
+    local text1 = wx.wxTextCtrl(frame, -1, "Pane 1 - sample text", wx.wxDefaultPosition, wx.wxSize(200,150), bit32.bor(wx.wxNO_BORDER,wx.wxTE_MULTILINE))
+    local text2 = wx.wxTextCtrl(frame, -1, "Pane 2 - sample text", wx.wxDefaultPosition, wx.wxSize(200,150), bit32.bor(wx.wxNO_BORDER,wx.wxTE_MULTILINE))
+    local text3 = wx.wxTextCtrl(frame, -1, "Main content window", wx.wxDefaultPosition, wx.wxSize(200,150), bit32.bor(wx.wxNO_BORDER,wx.wxTE_MULTILINE))
+
+    auiManager:AddPane(text1, wx.wxLEFT, "Pane Number One")
+    auiManager:AddPane(text2, wx.wxBOTTOM, "Pane Number Two")
+    auiManager:AddPane(text3, wx.wxCENTER)
+
+    auiManager:Update()
     
     frame:Connect(wx.wxID_EXIT, wx.wxEVT_COMMAND_MENU_SELECTED, function(e) frame:Close(true) end)
     frame:Connect(69, wx.wxEVT_COMMAND_MENU_SELECTED, RunProgram)
@@ -76,4 +93,4 @@ main()
 -- otherwise the wxLua program will exit immediately.
 -- Does nothing if running from wxLua, wxLuaFreeze, or wxLuaEdit since the
 -- MainLoop is already running or will be started by the C++ program.
-wx.wxGetApp():MainLoop()
+--wx.wxGetApp():MainLoop()
