92712e4d9a7f3584ece242ac37cb6c5142397756
Brandon Schmeidler <brandon.bsgstudio@gmail.com>
2019-03-01 18:23:38 -0600

    Packaged everything up into an App namespace.
    Setup a basic console output
    Setup a resource tree that is useless so far but it's there
    Setup a way to generate Window IDs for all the wx junk without standard IDs
    Source is a lot cleaner now

--- main.lua
+++ main.lua
@@ -1,96 +1,142 @@
 -----------------------------------------------------------------------------
--- Name:        Love2D Editor
+-- Name:       Love2D Editor
 -- Purpose:    WSYIWYG Editor for Love2D
--- Author:      Brandon Schmeidler
--- Modified by:
--- Created:     2/28/2019
+-- Author:     Brandon Schmeidler
+-- Created:    2/28/2019
 -- RCS-ID:     I don't know what this is
--- Copyright:   (c) 2019 B Schmeidler. All rights reserved.I guess.
--- Licence:     wxWidgets licence
+-- Copyright:  (c) 2019 Brandon Schmeidler. All rights reserved. I guess.
+-- License:    wxWidgets license
 -----------------------------------------------------------------------------
 
--- Load the wxLua module, does nothing if running from wxLua, wxLuaFreeze, or wxLuaEdit
---package.cpath = package.cpath..";./?.dll;./?.so;../lib/?.so;../lib/vc_dll/?.dll;../lib/bcc_dll/?.dll;../lib/mingw_dll/?.dll;"
---require("wx")
---local Project = require("Project")
-
-frame = nil
-runningPID = 0
-
-function ProgramStopped()
-    print("Program stopped successfully -- PID: " .. tostring(runningPID))
-end
-
-function StopProgram()
-    if (wx.wxProcess.Exists(runningPID)) then 
-        local killRet = wx.wxKill(runningPID, wx.wxSIGKILL)
-    else
-        print("No Program Running")
-    end
-end
-
-function RunProgram()
-    if (wx.wxProcess.Exists(runningPID)) then
-        StopProgram()
+function NewApp()
+    
+    local IDCOUNTER  = wx.wxID_HIGHEST+1
+    local function NewID() 
+        local r = IDCOUNTER
+        IDCOUNTER = IDCOUNTER + 1
+        return r
     end
 
-    runningPID = wx.wxExecute("love", wx.wxEXEC_ASYNC, wx.wxProcess(frame))
-    if (runningPID > 0) then
-        print("Program started succesfully -- PID: " .. tostring(runningPID))
-    else
-        print("Something went wrong. Make sure you have Love2D installed on your system and added to PATH")
+    local App = {
+        Frame = nil, 
+        AuiManager = nil,
+        ID = {
+            MainFrame = NewID(),
+            New = wx.wxID_NEW,
+            Exit = wx.wxID_EXIT,
+            Run = NewID(),
+            Stop = NewID(),
+            Console = NewID(),
+            TextEditor = NewID(),
+            ResourceTree = NewID()
+        },
+        Pane = {
+            Console = nil,
+            TextEditor = nil,
+            ResourceTree = nil
+        },
+        PaneInfo = {
+            Console = wxaui.wxAuiPaneInfo(),
+            TextEditor = wxaui.wxAuiPaneInfo(),
+            ResourceTree = wxaui.wxAuiPaneInfo()
+        },
+        RunningPID = 0
+    }
+
+    App.PaneInfo.Console:Name("Console")
+    App.PaneInfo.Console:Caption("Output")
+    App.PaneInfo.Console:Bottom()
+
+    App.PaneInfo.TextEditor:Name("Text Editor")
+    App.PaneInfo.TextEditor:Caption("Text Editor")
+    App.PaneInfo.TextEditor:Center()
+
+    App.PaneInfo.ResourceTree:Name("Resource Tree")
+    App.PaneInfo.ResourceTree:Caption("Resource Tree")
+    App.PaneInfo.ResourceTree:Right()
+
+    function App:OutputLog(msg)
+        self.Pane.Console:AppendText("\n"..msg)
     end
-end
 
-function CreateMenuBar()
-    local fileMenu = wx.wxMenu()
-    fileMenu:Append(wx.wxID_NEW, "New", "Start a new project")
-    fileMenu:Append(wx.wxID_EXIT, "&Exit\tAlt+F4","Quit the program")
+    function App:OnProgramStop()
+        self:OutputLog("Program stopped successfully -- PID: " .. tostring(self.RunningPID))
+    end
 
-    local projectMenu = wx.wxMenu()
-    projectMenu:Append(69, "Run","Run the program")
-    projectMenu:Append(70, "Stop","Stop the program")
+    function App:StopProgram()
+        if (wx.wxProcess.Exists(self.RunningPID)) then 
+            local killRet = wx.wxKill(self.RunningPID, wx.wxSIGKILL)
+        else
+            self:OutputLog("No Program Running")
+        end
+    end
 
-    local menuBar = wx.wxMenuBar()
-    menuBar:Append(fileMenu,"File")
-    menuBar:Append(projectMenu,"Project")
+    function App:RunProgram()
+        if (wx.wxProcess.Exists(self.RunningPID)) then
+            self:StopProgram()
+        end
+    
+        self.RunningPID = wx.wxExecute("love", wx.wxEXEC_ASYNC, wx.wxProcess(self.Frame))
+        if (self.RunningPID > 0) then
+            self:OutputLog("Program started succesfully -- PID: " .. tostring(self.RunningPID))
+        else
+            self:OutputLog("Something went wrong. Make sure you have Love2D installed on your system and added to PATH")
+        end
+    end
 
-    return menuBar
+    function App:Run()
+        self.Frame = wx.wxFrame( wx.NULL, self.ID.MainFrame, "Love2D Editor", wx.wxDefaultPosition, wx.wxSize(640,480), wx.wxDEFAULT_FRAME_STYLE )
+        
+        self.Frame:SetMenuBar((function()
+            local fileMenu = wx.wxMenu()
+            fileMenu:Append(self.ID.New, "New", "Start a new project")
+            fileMenu:Append(self.ID.Exit, "&Exit\tAlt+F4","Quit the program")
+
+            local projectMenu = wx.wxMenu()
+            projectMenu:Append(self.ID.Run, "Run","Run the program")
+            projectMenu:Append(self.ID.Stop, "Stop","Stop the program")
+
+            local menuBar = wx.wxMenuBar()
+            menuBar:Append(fileMenu,"File")
+            menuBar:Append(projectMenu,"Project")
+
+            return menuBar
+        end)())
+
+        self.AuiManager = wxaui.wxAuiManager(self.Frame)
+
+        self.Pane.TextEditor = wx.wxTextCtrl(self.Frame, self.ID.TextEditor, "", wx.wxDefaultPosition, wx.wxSize(640,480), bit32.bor(wx.wxNO_BORDER,wx.wxTE_MULTILINE))
+        
+        self.Pane.Console = wx.wxTextCtrl(self.Frame, self.ID.Console, "Love2D Editor <VERSION>", wx.wxDefaultPosition, wx.wxSize(640,480), bit32.bor(wx.wxNO_BORDER,wx.wxTE_MULTILINE))
+        self.Pane.Console:SetEditable(false)
+
+        self.Pane.ResourceTree = wx.wxGenericDirCtrl(self.Frame, self.ID.ResourceTree)
+
+        self.AuiManager:AddPane(self.Pane.TextEditor, self.PaneInfo.TextEditor)
+        self.AuiManager:AddPane(self.Pane.Console, self.PaneInfo.Console)
+        self.AuiManager:AddPane(self.Pane.ResourceTree, self.PaneInfo.ResourceTree)
+
+        self.AuiManager:Update()
+
+        self.Frame:Connect(self.ID.Exit, wx.wxEVT_COMMAND_MENU_SELECTED, function(e) self:Shutdown() end)
+        self.Frame:Connect(self.ID.Run, wx.wxEVT_COMMAND_MENU_SELECTED, function(e) self:RunProgram() end)
+        self.Frame:Connect(self.ID.Stop, wx.wxEVT_COMMAND_MENU_SELECTED, function(e) self:StopProgram() end)
+        self.Frame:Connect(wx.wxEVT_END_PROCESS, function(e) self:OnProgramStop() end)
+
+        self.Frame:Show(true)
+    end
+
+    function App:Shutdown()
+        self:OutputLog("Love2D Shutting down")
+        self.Frame:Close(true)
+    end
+
+    return App
 end
 
 function main()
-    
-    -- create the frame window
-    frame = wx.wxFrame( wx.NULL, wx.wxID_ANY, "Love2D Editor",
-                        wx.wxDefaultPosition, wx.wxSize(640,480),
-                        wx.wxDEFAULT_FRAME_STYLE )
-    
-    frame:SetMenuBar(CreateMenuBar())
-
-    local auiManager = wxaui.wxAuiManager(frame)
-
-    local text1 = wx.wxTextCtrl(frame, -1, "Pane 1 - sample text", wx.wxDefaultPosition, wx.wxSize(200,150), bit32.bor(wx.wxNO_BORDER,wx.wxTE_MULTILINE))
-    local text2 = wx.wxTextCtrl(frame, -1, "Pane 2 - sample text", wx.wxDefaultPosition, wx.wxSize(200,150), bit32.bor(wx.wxNO_BORDER,wx.wxTE_MULTILINE))
-    local text3 = wx.wxTextCtrl(frame, -1, "Main content window", wx.wxDefaultPosition, wx.wxSize(200,150), bit32.bor(wx.wxNO_BORDER,wx.wxTE_MULTILINE))
-
-    auiManager:AddPane(text1, wx.wxLEFT, "Pane Number One")
-    auiManager:AddPane(text2, wx.wxBOTTOM, "Pane Number Two")
-    auiManager:AddPane(text3, wx.wxCENTER)
-
-    auiManager:Update()
-    
-    frame:Connect(wx.wxID_EXIT, wx.wxEVT_COMMAND_MENU_SELECTED, function(e) frame:Close(true) end)
-    frame:Connect(69, wx.wxEVT_COMMAND_MENU_SELECTED, RunProgram)
-    frame:Connect(70, wx.wxEVT_COMMAND_MENU_SELECTED, StopProgram)
-    frame:Connect(wx.wxEVT_END_PROCESS, ProgramStopped)
-    -- show the frame window
-    frame:Show(true)
+    EditorApp = NewApp()
+    EditorApp:Run()
 end
 
 main()
-
--- Call wx.wxGetApp():MainLoop() last to start the wxWidgets event loop,
--- otherwise the wxLua program will exit immediately.
--- Does nothing if running from wxLua, wxLuaFreeze, or wxLuaEdit since the
--- MainLoop is already running or will be started by the C++ program.
---wx.wxGetApp():MainLoop()
