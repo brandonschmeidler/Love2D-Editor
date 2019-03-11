f7ff80156b06d06f08b5ec1fc26b47addb560c31
Brandon Schmeidler <brandon.bsgstudio@gmail.com>
2019-03-11 00:58:21 -0500

    EditorViewport now has items and functions to add items, remove items and check if items exist.
    This is probably a terrible way to do things but items are nothing more than a container that states what the shape is and it's parameters
    
    Circle - x,y,radius
    Line - x1,y1,x2,y2
    Polygon - points = { {x1,y1},{x2,y2},...,{x8,y8} }
    
    Polygon will only support 8 vertices so it can be compatible with box2D shape vertex limitation.

--- main.lua
+++ main.lua
@@ -17,20 +17,25 @@ local ScriptEditorNotebook = require("src.ScriptEditorNotebook")
 function main()
     frame = wx.wxFrame(wx.NULL, IdManager:NewId("Frame"), "Love2D Editor",wx.wxDefaultPosition, wx.wxSize(640,480))
 
-    editorNotebook = ScriptEditorNotebook(frame,IdManager:NewId("EditorNotebook"))
+    --editorNotebook = ScriptEditorNotebook(frame,IdManager:NewId("EditorNotebook"))
     browse = ContentBrowser(frame, IdManager:NewId("ContentBrowser"))
     output = ConsoleOutput(frame, IdManager:NewId("ConsoleOutput"))
     viewport = EditorViewport(frame, IdManager:NewId("EditorViewport"))
 
+    viewport:AddItem("origin",EditorViewport.ItemType.Circle,{x=0, y=0, radius=3})
+    viewport:AddItem("circle",EditorViewport.ItemType.Circle,{x=32, y=32, radius=32})
+    viewport:AddItem("line",EditorViewport.ItemType.Line, {x1=0, y1=0, x2=32, y2=32})
+    viewport:AddItem("poly",EditorViewport.ItemType.Polygon, {points={ {0,0},{32,0},{32,32},{0,32} }})
+
     auiManager = wxaui.wxAuiManager(frame)
-    auiManager:AddPane(editorNotebook:GetWidget(), wxaui.wxAuiPaneInfo():Name("Editors"):CaptionVisible(false):Center())
+    --auiManager:AddPane(editorNotebook:GetWidget(), wxaui.wxAuiPaneInfo():Name("Editors"):CaptionVisible(false):Center())
     auiManager:AddPane(browse:GetWidget(), wxaui.wxAuiPaneInfo():Name("ContentBrowser"):Caption("Content"):Right())
     auiManager:AddPane(output:GetWidget(), wxaui.wxAuiPaneInfo():Name("ConsoleOutput"):Caption("Console"):Bottom())
     auiManager:AddPane(viewport:GetWidget(), wxaui.wxAuiPaneInfo():Name("EditorViewport"):CaptionVisible(false):Center())
     auiManager:Update()
 
     browse:SetRoot(wx.wxStandardPaths.Get():GetResourcesDir() .. "\\TestProject")
-    editorNotebook:AddPage(wx.wxStandardPaths.Get():GetResourcesDir() .. "\\TestProject\\main.lua")
+    --editorNotebook:AddPage(wx.wxStandardPaths.Get():GetResourcesDir() .. "\\TestProject\\main.lua")
     output:Print("Love2D Editor Initialized")
 
     frame:Show(true)
--- src/EditorViewport.lua
+++ src/EditorViewport.lua
@@ -1,7 +1,19 @@
 local wxWidget = require("src.wxWidget")
 local common = require("lib.common")
 
-local EditorViewport = wxWidget:extend("EditorViewport")
+--local EditorViewportItem = class("EditorViewportItem")
+local function EditorViewportItem(type,params)
+   return { type = type, params = params }
+end
+
+
+local EditorViewport = wxWidget:extend("EditorViewport", {
+    ItemType = {
+        Circle = 1,
+        Line = 2,
+        Polygon = 3
+    }
+})
 
 local function bool_to_axis(bool)
     if (bool) then return 1 end
@@ -19,8 +31,7 @@ function EditorViewport:init(frame,id)
 
     self.items = {}
 
-
-    self.widget:Connect(wx.wxEVT_MIDDLE_DOWN, function(e) print(self.scrollX,self.scrollY) self:OnMiddleDown() end)
+    self.widget:Connect(wx.wxEVT_MIDDLE_DOWN, function(e) self:OnMiddleDown() end)
 
     self.widget:Connect(wx.wxEVT_MIDDLE_UP, function(e) self:OnMiddleUp() end)
 
@@ -29,12 +40,23 @@ function EditorViewport:init(frame,id)
     self.widget:Connect(wx.wxEVT_PAINT, function(e) self:OnPaint() end)
 end
 
+function EditorViewport:AddItem(name,type,params)
+    local item = EditorViewportItem(type,params)
+    self.items[name] = item
+end
+
+function EditorViewport:RemoveItem(name)
+    self.items[name] = nil
+end
+
+function EditorViewport:HasItem(name)
+    return self.items[name] ~= nil
+end
+
 function EditorViewport:OnMiddleDown()
     local mx,my = self.widget:ScreenToClient(wx.wxGetMousePosition()):GetXY()
     self.mouseGrabX = mx+self.scrollX
     self.mouseGrabY = my+self.scrollY
-
-    print("MOUSE DOWN",self.mouseGrabX,self.mouseGrabY)
     self.scrolling = true
     self.widget:CaptureMouse()
 end
@@ -46,39 +68,12 @@ function EditorViewport:OnMiddleUp()
     end
 end
 
--- function EditorViewport:OnLeaveWindow()
---     if (self.scrolling) then
---         --self.scrolling = false
---         local mx,my = self.widget:ScreenToClient(wx.wxGetMousePosition()):GetXY()
---         local size = self.widget:GetClientSize()
---         local ww = size:GetWidth()
---         local wh = size:GetHeight()
---         print(mx,my,ww,wh)
---         if (mx < 0 or mx > ww) then
---             local axis = bool_to_axis(mx < 0)
---             local offset = axis * ww
---             mx = mx + offset
---             self.mouseGrabX = self.mouseGrabX + offset
---             self.widget:WarpPointer(mx,my)
---         end
-
---         if (my < 0 or my > wh) then
---             local axis = bool_to_axis(my < 0)
---             local offset = axis * wh
---             my = my + offset
---             self.mouseGrabY = self.mouseGrabY + offset
---             self.widget:WarpPointer(mx,my)
---         end
---     end
--- end
-
 function EditorViewport:OnMotion()
     if (self.scrolling) then
         local mx,my = self.widget:ScreenToClient(wx.wxGetMousePosition()):GetXY()
         local size = self.widget:GetClientSize()
         local ww = size:GetWidth()
         local wh = size:GetHeight()
-        print(mx,my,ww,wh)
         if (mx < 0 or mx > ww) then
             local axis = bool_to_axis(mx < 0)
             local offset = axis * ww
@@ -101,23 +96,43 @@ function EditorViewport:OnMotion()
 end
 
 function EditorViewport:OnPaint()
-    local dc = wx.wxBufferedPaintDC(self.widget)
+    local dc = wx.wxAutoBufferedPaintDC(self.widget)
     local size = self.widget:GetClientSize()
     local ww = size:GetWidth()
     local wh = size:GetHeight()
     dc:Clear()
 
-    for i=1,20 do
-        for j=1,20 do
-            local r = 20
-            local p = 5
-            local x = i * (r * 2) - self.scrollX
-            local y = j * (r * 2) - self.scrollY
-            if (x >= 0 and y >= 0 and ww >= x and wh >= y) then
-                dc:DrawCircle(x,y,r)
+    dc:SetBrush(wx.wxTRANSPARENT_BRUSH)
+
+    --local mx,my = self.widget:ScreenToClient(wx.wxGetMousePosition()):GetXY()
+    --dc:DrawText(tostring(mx - self.scrollX) .. "," .. tostring(my - self.scrollY),1,1)
+
+    for _,item in pairs(self.items) do
+        local type = item.type
+        if (type == EditorViewport.ItemType.Circle) then
+            dc:DrawCircle(item.params.x - self.scrollX, item.params.y - self.scrollY, item.params.radius)
+        elseif (type == EditorViewport.ItemType.Line) then
+            dc:DrawLine(item.params.x1 - self.scrollX, item.params.y1 - self.scrollY, item.params.x2 - self.scrollX, item.params.y2 - self.scrollY)
+        else -- if polygon
+            local points = {}
+            for i,point in ipairs(item.params.points) do
+                points[i] = {point[1] - self.scrollX, point[2] - self.scrollY}
             end
+            dc:DrawPolygon(points)
         end
     end
+
+    -- for i=1,20 do
+    --     for j=1,20 do
+    --         local r = 20
+    --         local p = 5
+    --         local x = i * (r * 2) - self.scrollX
+    --         local y = j * (r * 2) - self.scrollY
+    --         if (x >= 0 and y >= 0 and ww >= x and wh >= y) then
+    --             dc:DrawCircle(x,y,r)
+    --         end
+    --     end
+    -- end
 
     dc:delete()
 end
